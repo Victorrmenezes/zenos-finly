4eaf3bd1c5aa59132ea9f2c29787a89d215a01cc
JVictor <victorrmenezes9@gmail.com>
2025-10-26 21:37:58 -0300

    category model

--- backend/cash_flow/models.py
+++ backend/cash_flow/models.py
@@ -60,12 +60,8 @@ class CreditCardInvoice(models.Model):
 
 
 class Category(models.Model):
-    TYPE_CHOICES = [
-        ('INCOME', 'Income'),
-        ('EXPENSE', 'Expense'),
-    ]
+
     name = models.CharField(max_length=100)
-    type = models.CharField(max_length=10, choices=TYPE_CHOICES)
 
     def __str__(self):
         return f"{self.name}"
